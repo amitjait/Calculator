515849614004aca254fa21f9a2a7278fbc860c0d
amitjait <amit.rimit591@gmail.com>
2022-07-19 19:56:12 +0530

    updating

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const equalBtn = document.getElementById('equal');
 const clear = document.getElementById('clear');
 let val = document.getElementById("input").value;
 var btn = document.getElementsByClassName("btn");
+const back = document.getElementById("back");
 
 console.log(btn.length);
 // function myClick(){
@@ -23,7 +24,13 @@ console.log(btn[1].value);
 for(var i=0; i<btn.length; i++){
     let btnVal = btn[i].value;
     btn[i].addEventListener('click', () => {
-        
         document.getElementById("input").value += btnVal;
     });
 }
+
+back.addEventListener('click', ()=>{
+    let backSpace = document.getElementById("input").value;
+    if(backSpace.length != 0){
+        document.getElementById("input").value = backSpace.substring(0, backSpace.length-1);
+    }
+});
